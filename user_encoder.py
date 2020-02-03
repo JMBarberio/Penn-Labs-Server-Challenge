cd44ac5b6741d6fd54183775be4c892682d448d9
Joseph Maximilian Barberio <josephbarberio99@gmail.com>
2020-02-03 18:23:30 -0500

    Removed

--- user_encoder.py
+++
@@ -1,9 +0,0 @@
-from json import *
-from user import User
-
-class UserEncoder(JSONEncoder):
-     def default(self, obj):
-        if isinstance(obj, User):
-            return {"User Name: " : obj.get_user_name(),
-            "User Clubs" : obj.get_user_clubs()}
-        return JSONEncoder.default(self, obj)
